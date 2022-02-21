c7cb0f0a8930d1b99da43e41ca2fcf7426ac9418
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-21 12:15:17 -0800

    supabase client added, initial commit

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://sjdlsxmnlasfuesvmebz.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZGxzeG1ubGFzZnVlc3ZtZWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0ODAsImV4cCI6MTk1OTkxNzQ4MH0.SGC6ShwKbSPLUcnWYRO3o7KQbJaV16eTy-AmGphLBh4';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
